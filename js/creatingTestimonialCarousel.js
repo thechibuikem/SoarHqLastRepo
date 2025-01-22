14ad6197ca58a44834c4424337f910a2617ffb39
Chukwuemeka David <chukwuemekadavid2007@gmail.com>
2025-01-22 05:30:32 +0100

    fixing book

--- js/creatingTestimonialCarousel.js
+++ js/creatingTestimonialCarousel.js
@@ -1,9 +1,9 @@
-TestCardWrap = document.querySelector(".figureWrap");
-TestCard1 = document.querySelector("#testimonialBlock");
+let TestCardWrap = document.querySelector(".figureWrap");
+let TestCard1 = document.querySelector("#testimonialBlock");
 
 for(let i = 0; i < 3; i++){
  let TestCardClone = TestCard1.cloneNode(true);
- TestCardClone.classlist.add(`TestCard${i}`, testcard)
+ TestCardClone.classList.add(`TestCard${i}`, `testcard`)
  TestCardWrap.appendChild(TestCardClone)
 }
 
