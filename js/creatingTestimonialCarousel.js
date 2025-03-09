02f2ba4745cd992faab234f4b5241d6838f9e111
Chukwuemeka David <chukwuemekadavid2007@gmail.com>
2025-03-09 16:59:10 +0100

    latest

--- js/creatingTestimonialCarousel.js
+++ js/creatingTestimonialCarousel.js
@@ -1,58 +1,168 @@
-const filler = [
+// array of objects to fill our slidelist
+const slideListFiller = [
   {
-    id:1,
-    image:"images/girl.svg",
-    name:"fransisca",
-    testimonial:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eveniet nulla ea nihil animi saepe alias illum nobis quidem sit."
+    id: 1,
+    image: "images/girl.svg",
+    name: "Success Nwankpa",
+    testimony: `Good evening, Excellencies! I am Excellent Success, Success Nwankpa.I just concluded my discipleship call with the steward, @Excellent Nancy, and it was truly transformative.I used the word "transformative" because just a few minutes before the call, I was feeling down, there is a power outage, it left my laptop unusable. I have important tasks to complete to meet upcoming timelines, and I was also troubled by some family issues. However, joining the call opened my eyes to hope and strengthened me by the grace of God.During our conversation, Excellent Nancy shared some of her undergraduate experiences, which resonated deeply with me. I left the call feeling uplifted and strengthened in God's word; I even received insights to share with my twin sister.I am continually grateful for this community of Godliness and excellence.God bless you, @Excellent Nancy; may you receive more of God's grace ma.God bless us all!`,
   },
   {
-    id:2,
-    image:"images/girl.svg",
-    name:"fransisca",
-    testimonial:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eveniet nulla ea nihil animi saepe alias illum nobis quidem sit."
-  } ,
-   {
-    id:3,
-    image:"images/girl.svg",
-    name:"fransisca",
-    testimonial:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eveniet nulla ea nihil animi saepe alias illum nobis quidem sit."
-  },
-  {
-    id:4,
-    image:"images/girl.svg",
-    name:"fransisca",
-    testimonial:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eveniet nulla ea nihil animi saepe alias illum nobis quidem sit."
-  }
-]
-;
-filler.forEach(item => {
-  slideList[0]
-})
+    id: 2,
+    image: "images/girl.svg",
+    name: "Somtochukwu Okoroafor",
 
+    testimony: `
+    Good evening, Excellencies,     
+    I'm Excellent Somtochukwu I just concluded my discipleship call with the steward, @Excellent Nancy, and it was refreshing and weight lifting.
+    That moment you've been there for other people and then you don't quite find someone who can really resonate with the challenges you're going through. Speaking with Excellent Nancy, I felt lighter that someone could relate with this phase of my life and I'm not weird after all.
+    In the course of the conversation, Excellent Nancy shared some of her experiences and how The Holy Spirit, as well as practical steps she took to get through the challenges and came out stronger
+    The call reminded me to continue keeping my eyes on Jesus and that which He is doing in and through me, and that my walk with the Holy Spirit is peculiar and unique to just me alone. And the prayers were so timely.
+    Thank you so much @Excellent Nancy for your labour of love and stewardship over us .
+    Grace and strength is multiplied to you. God bless you Ma
+    `,
+  },
+
+  {
+    id: 3,
+    image: "images/girl.svg",
+    name: "Udoka Ngozichukwu",
+    testimony: `
+    Good morning Excellencies
+    I'm Excellent Ngozichukwu
+    I had my discipleship call with the steward @Excellent Nancy about a week ago and I can say for a fact that I was blessed.
+    Every word she spoke was what I needed to hear at the moment because I was a bit overwhelmed and distracted by the recent happenings around me but at the end of the call I was encouraged.
+    I was encouraged to not lose consciousness of God regardless of how things might be and to carry Him along in all my activities and this has been my reality since then.
+    I want to say a big thank you @Excellent Nancy for all you do ma.
+    God bless you greatly ma
+
+    `,
+  },
+
+  {
+    id: 4,
+    image: "images/girl.svg",
+    name: "Chukwuma-Emeaso Faith Adaeze",
+    testimony: `
+    Good Afternoon Excellencies
+    I am Excellent Adaeze.
+    My discipleship call with @Excellent Nancy was like spring. I have been going through some things which have been bugging me for a long while. It made me judge myself harshly but the call gave me hope and showed me that I will be fine if I just do the needful and trust God.
+    Thank you @Excellent Nancy. I truly appreciate the effort you put towards us here. God bless you and everyone at SOAR HQ.
+    `,
+  },
+  {
+    id: 5,
+    image: "images/girl.svg",
+    name: "Ugochi Oguikpe",
+    testimony: `
+    Hello Excellencies,
+    I am Excellent Ugochi.
+    My discipleship call with the Steward, @Excellent Nancy, was truly an answered prayer. It revealed my blind spots and highlighted essential virtues that are crucial for this season of my life.
+    The call validated Divine instructions I had been wrestling with, showing me how even unconventional means can be mightily used by God for His glory.
+    I've come to realize that certain aspects of my walk with God and my life have significance I hadn't recognized before. Because of that, I had underestimated their importance and the messages they held. This call was a moment of revival and reflection for me, and I deeply treasure every word I received.
+    The best way I can express my gratitude to God and to our Steward is by fully implementing every instruction and confirmation that came from the call, and I commit to doing just that.
+    `,
+  },
+  {
+    id: 6,
+    image: "images/girl.svg",
+    name: "Faith Ndubuisi N.",
+    testimony:`
+    Good day Excellencies,
+    I am Excellent Faith.
+    The Call I had with @Excellent Nancy was more of a re-assurance!
+    This year for me was filled with so many uncertainties and in the call We spoke about some of these and the assignment that God gave to me and how I was really unsure of how to go about it.
+    I tried to execute some of these assignments to the best of my abilities but the issue is that there's always the constant feeling of "You are not even qualified to do this",  " are you really doing this for humanity or Christianity sake or for show offs"? Just many questions like that...
+    And this call reassured me that "God doesn't call the qualified, He calls the unqualified and then Qualifies them".
+    I am reminded that by the virtue of being a carrier of God's assignment, it's already a qualification to go out there and be Excellent!
+    I am more confident and even more open minded with the knowledge that fulfilling purpose is the calling and if there's a purpose, if there's a calling, then grace is multiplied!
+    Thank you Steward, @Excellent Nancy for the call and the prayers. God bless you ma.
+      `,
+  },
+  {
+    id: 7,
+    image: "images/girl.svg",
+    name: "Chinaza Martha",
+    testimony:`
+    As far as my eyes can see."
+    This statement keeps ringing in my mind everytime I feel confused.
+    Good evening Excellencies
+    First of all, I want to say thank you @Excellent Nancy for the call yesterday.
+    It was a supernatural source of hope and help to a tired and confused me.
+    Life after school hasn't been as beautiful as it seemed to me when I was an undergraduate. A lot of thoughts, advices and many other things. Today it seems like I'm on the right path, tomorrow, it's as if I'm on a different path all along. The pressure and all,
+    Thank God for Using @Excellent Nancy to remind me of He who lives in me. His steadfast love for me never ceases.
+    And for anything out there, serving as obstacle or limitation or pressure, I'll pave my way through Him who strengthens me. Taking them out one after another.
+    That beautiful statement she said to me from the Bible, "As far as my eyes can see." Out there to get as much as my eyes can see, farther and farther... Amen
+    Alot to say but I'll stop here.
+    `
+  },
+  {
+    id: 8,
+    image: "images/girl.svg",
+    name: "Jane Daniel",
+    testimony:
+    `Hello Excellencies,
+    I am Excellent Jane.
+    I just had a discipleship call with the steward @Excellent Nancy. This call was like a relief to me, I had so much burden on my heart but it’s alright because I’m God’s light and God’s light shines the brightest.
+    We looked into a bible verse Hebrews 4:16 which says “Let us therefore come boldly to the throne of grace, that we may obtain mercy and find grace to help in time of need". We should not ask for mercy or for the grace of God but we should receive it with confidence.
+    I was encouraged that God has great plans for me and I will become that which God wants me to be.
+    I want to say Thank you @Excellent Nancy for your kind words and prayers.
+    God bless you ma. ♥️
+    `,
+  },
+  {
+    id: 9,
+    image: "images/girl.svg",
+    name: "Jayeola Oluwatoyin Eunice",
+    testimony:
+    `Hello Excellencies,
+    I am Excellent Eunice.
+    A week tomorrow I had my discipleship call with the steward @Excellent Nancy, I must say the call was needed and it’s nice to have to talk to someone who sees from your perspective or has been at the stage where you’re at at the moment. 
+    I remember we spoke about building a relationship with the Holy Spirit and it made me realize what I’ve been doing wrong instead of trying to emotionalize my relationship with God than building it by constantly being in his presence instead of basing his presence on feeling.
+    The call was really an eye and mind opening one and exactly what I needed and I’m really grateful for that.
+    I want to say Thank you @Excellent Nancy for your kind words and prayers and thank you to all the beautiful souls here
+    God bless you all. ♥️
+    `,
+  },
+
+];
 
 let TestCardWrap = document.querySelector(".figureWrap");
 // card 1 declaration
 let TestCard1 = document.querySelector("#testimonialBlock");
 
-for (let i = 0; i < 3; i++) {
+// empty array to hold the cloned cards
+let clonedTestCardArray = [];
+
+// cloning of the testimonial cards
+for (let i = 1; i < 9; i++) {
   let TestCardClone = TestCard1.cloneNode(true);
-  TestCardClone.classList.add(`TestCard${i}`, `testcard`);
+  TestCardClone.classList.add(`testCard${i}`, `testcard`);
+  // pushing the cloned cards into the ClonedTestCardArray
+  clonedTestCardArray.push(TestCardClone);
+
+  // pushing the cloned test cards into the DOM
   TestCardWrap.appendChild(TestCardClone);
 }
 
-// declaration of the essentials
-let slideCon = document.querySelectorAll(".invi");
-// getting array of slides from html collection
-let slideList = Array.from(slideCon);
+// getting array of slides from html collection, we are doing this because now we have 4 invi classes instead of 1
+let slideList = Array.from(document.querySelectorAll(".invi"));
+
+// manipulating array contents dynamically
+slideList.forEach((element, index) => {
+  let name = element.querySelector("#tBlockGrp2 h3");
+  let testimony = element.querySelector("#tBlockGrp2 h4");
+
+  if (name) name.textContent = slideListFiller[index].name;
+  if (testimony) testimony.textContent = slideListFiller[index].testimony;
+});
+
+// testing if my arrays are in check
+// clonedTestCardArray[0].style.backgroundColor = "blue";
+// clonedTestCardArray[1].style.backgroundColor = "navy";
+// clonedTestCardArray[2].style.backgroundColor = "indigo";
+
+// what makes the slider work
 let clickCount = 0;
-
-
-slideList[1].style.backgroundColor = "red"
-slideList[2].style.backgroundColor = "orange"
-slideList[3].style.backgroundColor = "yellow"
-
-
-
 lButton = document.querySelector("#left");
 rButton = document.querySelector("#right");
 
@@ -64,37 +174,33 @@ function hideall() {
 }
 // function to display just one element in the array
 let showOne = () => {
-    slideList[clickCount].style.display = "flex";
-  }
+  slideList[clickCount].style.display = "flex";
+};
 
-hideall()
-showOne()
-
-let leftClick = () => {
-hideall()
-
-clickCount--;
-if (clickCount < 0){
-  clickCount = slideList.length - 1};
+// calling the functions
+hideall();
 showOne();
 
-}
+let leftClick = () => {
+  hideall();
 
+  clickCount--;
+  if (clickCount < 0) {
+    clickCount = slideList.length - 1;
+  }
+  showOne();
+};
 
 let rightClick = () => {
-hideall();
+  hideall();
   clickCount++;
-  if (clickCount > slideList.length - 1
-  )
-  {clickCount = 0};
-showOne()
+  if (clickCount > slideList.length - 1) {
+    clickCount = 0;
   }
+  showOne();
+};
 
-
-
-
-
-  lButton.addEventListener("click",leftClick)
-rButton.addEventListener("click",rightClick)
+lButton.addEventListener("click", leftClick);
+rButton.addEventListener("click", rightClick);
 
 // hideall();
